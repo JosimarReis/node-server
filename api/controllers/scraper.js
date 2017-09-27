ee3df3c84d524a11bd2fa5c086f5bc8ee165994c
Josimar Reis <josimargeraldoreis@gmail.com>
2017-09-26 22:10:29 -0300

    Adicionado alguns comentarios nestes arquivos apenas para testar o git no Atom. :)

--- api/controllers/scraper.js
+++ api/controllers/scraper.js
@@ -11,12 +11,12 @@ var anuncio = require('./../models/anuncio');
 var AnuncioModel = require('./../models/anuncioModel');
 var AnunciosAntigosModel = require('./../models/anunciosAntigosModel');
 
-
+//aqui carrega os dados do site que ira nos fornecer os dados
 var mfRural = {
     nome:'MFRural',
     url_base:'http://www.mfrural.com.br/',
     url_busca:'http://www.mfrural.com.br/busca.aspx?palavras=',
-    url_separador: '&pg=', 
+    url_separador: '&pg=',
     termo: 'soja'
 };
 
@@ -34,7 +34,7 @@ var mongo_create_a_anuncio = function(anuncioModel){
         if(err)
             console.log("erro ao inserir anuncio. "+err)
     });
-    
+
 }
 
 
@@ -51,7 +51,7 @@ exports.postar = function(req, res){
 var iniciarColeta = function(site){
     var qtd =0;
     var url = site.url_busca+site.termo;
-    
+
     request(url,function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
@@ -59,8 +59,8 @@ var iniciarColeta = function(site){
              //colhe a quantidade de paginas a serem carregadas
             $('.pagination li').filter(function(){
                     var data = $(this);
-                    qtd = data.children().last().attr('href').split('&pg=')[1];  
-                    
+                    qtd = data.children().last().attr('href').split('&pg=')[1];
+
             });
 
            colher(site, qtd);
@@ -89,21 +89,21 @@ var getDetalhesAnuncios = function(pagina){
             var data = $(this);
             an.descricao = data.text();
         });
-        
+
         $('.detalhesMapa').filter(function(){
             var data = $(this);
             var temp = data.attr('onclick').split('=')[1].split('/');
             an.localizacao.cidade = temp[0].replace(/-/g," ");
             an.localizacao.uf = temp[1];
-           
+
         });
 
         $('.jcarousel-skin-pika li img').filter(function(){
             var data = $(this);
             an.imagem = data.attr('src');
-            
+
         });
-        
+
         an.anunciante.nome = "MFRural";
         an.anunciante.site = an.url;
         console.log(an);
@@ -126,15 +126,15 @@ exports.list_all_anuncios = function(req, res) {
     });
   };
 
-  
+
 var colher = function(site, quantidadePaginas){
     console.log("temos "+quantidadePaginas+" de paginas");
     var url = site.url_busca+site.termo;
         //busca a quantidade de paginas para percorrer a lista
-      
+
         var ps = [];
         var links = [];
-            
+
            //percorre pelas paginas e colhe os links
            for(var i=1; i<=quantidadePaginas; i++){
               var  options={
@@ -143,7 +143,7 @@ var colher = function(site, quantidadePaginas){
                     return cheerio.load(body);
                 }
                }
-             
+
           ps.push(rp(options)
            .then(function ($) {
                    $('.boxAnunciosTitulo').filter(function(){
@@ -155,13 +155,13 @@ var colher = function(site, quantidadePaginas){
 
         //executa uma promisse com todas as solicitacoes.
         Promisse.all(ps)
-        
+
         .then(function(){
             /**
             var arquivo =  new Date().getTime()+'.json';
             fs.writeFile(arquivo, JSON.stringify(links, null, 4), function(err){
              console.log('arquivo com links criados');
-             
+
             })
             */
 
@@ -169,7 +169,7 @@ var colher = function(site, quantidadePaginas){
             console.log("Lista 01 => "+links);
             var paginas = links.duplicates();
             console.log("Lista 02 => "+paginas);
-            
+
             paginas.forEach(function(link) {
                 if(validUrl.isHttpUri(link)){
                 var options = {
@@ -179,14 +179,14 @@ var colher = function(site, quantidadePaginas){
                     }};
                     console.log(options);
                 anuncios.push(rp(options).then(function ($) {
-                    
+
                         var an = anuncio;
                         an.url = link;
                         $('.detalhesTitulo').filter(function(){
                             var data = $(this);
                             an.titulo = data.attr('title');
                         });
-                
+
                         $('.produto-preco').filter(function(){
                             var data = $(this);
                             an.preco = data.text();
@@ -195,24 +195,24 @@ var colher = function(site, quantidadePaginas){
                             var data = $(this);
                             an.descricao = data.text();
                         });
-                        
+
                         $('.detalhesMapa').filter(function(){
                             var data = $(this);
                             var temp = data.attr('onclick').split('=')[1].split("'")[0].split('/');
                             an.localizacao.cidade = temp[0].replace(/-/g," ");
                             an.localizacao.uf = temp[1];
-                           
+
                         });
-                
+
                         $('.jcarousel-skin-pika li img').filter(function(){
                             var data = $(this);
                             an.imagem = data.attr('src');
-                            
+
                         });
-                        
+
                         an.anunciante.nome = "MFRural";
                         an.anunciante.site = an.url;
-                        
+
                         mongo_create_a_anuncio(AnuncioModel(an));
 
                     }).catch(err => console.log('erro na options ='+options.uri+' err++'+err))
@@ -221,25 +221,25 @@ var colher = function(site, quantidadePaginas){
             });
 
         Promisse.all(anuncios).catch(err => console.log("erro na promisse- salvar detalhes: "+err));
-        
+
 
         })
         .catch(err => console.log("erro na promisse: "+err));
-        
-   
+
+
   }
 
-  
+
 //copia base dados antiga salva em anunciosantigos
 // remove os anuncios existentes
 //inicia a coelta de novos anuncios.
 exports.mongo_bkp_all_anuncio = function(req, res){
-    
+
     AnuncioModel.find({}, function(err, anuncio) {
         if (err)
           res.send(err);
         //res.send(anuncio);
-        
+
         AnunciosAntigosModel({anuncios:anuncio}).save(function(err){
             if(!err){
                 //apaga os anuncios anteriores
@@ -264,7 +264,7 @@ exports.cidades = function(req, res){
               return cheerio.load(body);
           }
          }
-       
+
     ps.push(rp(options).then(function($){
 
     }).catch(function(err){
